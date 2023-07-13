12290bd0944dbca036e47b9d36d5f7eb201b58c1
biosisg <biosis.lanjanirina@icloud.com>
2023-07-14 00:54:46 +0300

    Prepopulate users and questions tables using model factories and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Question;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(3)->create()->each(function ($u) {
+            $u->questions()
+                ->saveMany(
+                    Question::factory(rand(1, 5))->make()
+                );
+        });
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
