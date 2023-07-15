a437f7e08a3c6dcdc2a6b069b17234e9661b9258
biosisg <biosis.lanjanirina@icloud.com>
2023-07-15 11:02:00 +0300

    Add delete functionality for the question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -35,8 +35,19 @@
                                 <div class="media-body">
                                     <div class="d-flex justify-content-between">
                                         <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                        <a href="{{ route('questions.edit', $question->id) }}"
-                                           class="btn btn-outline-info">Edit</a>
+                                        <div>
+                                            <a href="{{ route('questions.edit', $question->id) }}"
+                                               class="btn btn-outline-info">Edit</a>
+                                            <form action="{{ route('questions.destroy', $question->id) }}"
+                                                  method="post"
+                                                  class="form-delete">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-outline-danger"
+                                                        onclick="return confirm('Are you sure ?')">Delete
+                                                </button>
+                                            </form>
+                                        </div>
                                     </div>
                                     <p class="">
                                         <small>
