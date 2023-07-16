30298bcab462b3a08f3b7ec8fde020fe1d19ec54
biosisg <biosis.lanjanirina@icloud.com>
2023-07-16 18:08:16 +0300

    Design answers table schema and rename answers column to answers_count in the questions table

--- app/Policies/QuestionPolicy.php
+++ app/Policies/QuestionPolicy.php
@@ -21,6 +21,6 @@ class QuestionPolicy
      */
     public function delete(User $user, Question $question): bool
     {
-        return $user->id === $question->user_id && $question->answers < 1;
+        return $user->id === $question->user_id && $question->answers_count < 1;
     }
 }
